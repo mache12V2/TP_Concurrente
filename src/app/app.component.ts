9e81f674ee6c5767e836ff6fcacecbc62d688d25
Esteban Cabrera <86165162+EstebanCabreraArbizu@users.noreply.github.com>
2024-06-24 14:35:57 -0500

    Enlazandolo al main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, MainComponent],
+  template: '<app-main></app-main>'
 })
 export class AppComponent {
   title = 'api-tf-app';
