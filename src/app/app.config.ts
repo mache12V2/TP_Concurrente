1541b5413810c1a8d152751d0b6b24edd4c39c2f
Esteban Cabrera <86165162+EstebanCabreraArbizu@users.noreply.github.com>
2024-06-24 15:06:35 -0500

    Corrigiendo el error de htttp Client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { ApiService } from './api.service';
+import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
 	providers: [
+		provideHttpClient(),
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
 		provideClientHydration(),
